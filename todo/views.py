18a642332cbcf5f92424e831670ef204e757a635
Dizarimia <lillemor.sunnegard@outlook.com>
2022-11-11 14:07:19 +0000

    change html name

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse,
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return httpresponse("Hello!")
+    return HttpResponse("Hello!")
