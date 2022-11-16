a44c5d8a73d34764d13cd2275fc4d0947b3e6f1c
Dizarimia <lillemor.sunnegard@outlook.com>
2022-11-16 19:19:40 +0000

    add todo item

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-CSRF_TRUSTED_ORIGINS = ['https://*.mydomain.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.mydomain.com',
+    'https://*.127.0.0.1',
+    'https://8000-dizarimia-project4-kq4gbbyi2j1.ws-eu75.gitpod.io'
+]
 
 
 # Application definition
